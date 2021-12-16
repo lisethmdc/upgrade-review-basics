c0ba572eb06cda4aaa039ae5b0a7b3bb5f0156aa
lisethmdc <lismc1992@gmail.com>
2021-12-16 19:48:35 +0100

    Iteration#3

---
+++ Iteration3.js
@@ -0,0 +1,49 @@
+const users = [
+    {
+      name: "Manolo el del bombo",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 50 },
+        rain: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "Mortadelo",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 30 },
+        shower: { format: "ogg", volume: 55 },
+        train: { format: "mp3", volume: 60 },
+      },
+    },
+    {
+      name: "Super Lopez",
+      favoritesSounds: {
+        shower: { format: "mp3", volume: 50 },
+        train: { format: "ogg", volume: 60 },
+        firecamp: { format: "mp3", volume: 80 },
+      },
+    },
+    {
+      name: "El culebra",
+      favoritesSounds: {
+        waves: { format: "mp3", volume: 67 },
+        wind: { format: "ogg", volume: 35 },
+        firecamp: { format: "mp3", volume: 60 },
+      },
+    },
+  ]
+
+  let repeat = {}
+
+  for(let item of users) {
+      for(let data in item.favoritesSounds) {
+          if(repeat[data] === undefined) {
+              repeat[data] = 1
+          } else {
+              repeat[data] += 1
+          }
+        }
+    }
+    
+    console.log(repeat)
+   
