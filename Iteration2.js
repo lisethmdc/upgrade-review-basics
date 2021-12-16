acb76a4afbe8062a74617441f72d9cd1112e127d
lisethmdc <lismc1992@gmail.com>
2021-12-16 19:37:15 +0100

    Iteration#2

---
+++ Iteration2.js
@@ -0,0 +1,54 @@
+const users = [
+  {
+    name: "Manolo el del bombo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 50 },
+      rain: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "Mortadelo",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 30 },
+      shower: { format: "ogg", volume: 55 },
+      train: { format: "mp3", volume: 60 },
+    },
+  },
+  {
+    name: "Super Lopez",
+    favoritesSounds: {
+      shower: { format: "mp3", volume: 50 },
+      train: { format: "ogg", volume: 60 },
+      firecamp: { format: "mp3", volume: 80 },
+    },
+  },
+  {
+    name: "El culebra",
+    favoritesSounds: {
+      waves: { format: "mp3", volume: 67 },
+      wind: { format: "ogg", volume: 35 },
+      firecamp: { format: "mp3", volume: 60 },
+    },
+  },
+];
+
+
+let numVol = 0
+let totalVolume = 0
+
+for (var item of users) {
+    //console.log(item.favoritesSounds)
+    for(var data in item.favoritesSounds) {
+      totalVolume += item.favoritesSounds[data].volume
+      numVol++
+    }
+}
+
+console.log(totalVolume)
+console.log(numVol)
+
+
+let avgVolume = totalVolume / numVol
+
+console.log(avgVolume)
